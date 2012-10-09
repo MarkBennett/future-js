551fba752036a32242b8d8dcf9e0c45065287f78
Mark <mark@burmis.ca>
2012-10-08 20:24:14 -0400

    Add initial Dartlang compatability

---
+++ future.js
@@ -0,0 +1,44 @@
+(function() {
+    // Establish the root object, `window` in the browser, or `exports` on the server.
+    var root;
+    
+    if (typeof exports !== 'undefined') {
+        // We're using node
+        root = exports;
+    } else {
+        // We're in the browser
+        this['future'] = {};
+        root = this['future'];
+    }
+    
+    root.VERSION = "0.0.1";
+    
+    root.Completer = function() {
+        var _future,
+            completed_callbacks = [];
+        
+        // A private constructor for our future
+        function Future() {
+            this.then = function(callback) {
+                completed_callbacks.push(callback);
+                return this;
+            };
+        }
+        
+        // Return the future associated with this Completer
+        this.future = function() {
+            _future = _future || new Future();
+            
+            return _future;
+        };
+        
+        // Complete this Completer, calling any associated callbacks
+        this.complete = function() {
+            var i;
+            
+            for(i = 0; i < completed_callbacks.length; i++) {
+                completed_callbacks[i].apply(this, arguments);
+            }
+        };
+    };
+}).call(this);
---
+++ spec/future-spec.js
@@ -0,0 +1,51 @@
+var future = require('../');
+
+describe("Future-js", function() {
+    it("should declare a version", function() {
+        expect(future.VERSION).toEqual("0.0.1");
+    });
+    
+    
+    describe("Completer", function() {
+        var completer;
+        
+        beforeEach(function() {
+            completer = new future.Completer();
+        });
+        
+        it("should let you create a Future", function() {
+            expect(completer.future()).toBeDefined();
+        });
+        
+        it("should call any the Future's handler with the arguments to complete()", function() {
+            var future = completer.future(),
+                completed_args = [1, 2, 3],
+                handler_args;
+                
+            // Register a handler on the future
+            future.then(function() {
+                handler_args = Array.prototype.slice.call(arguments, 0);
+            });
+            
+            // Complete the completer
+            completer.complete.apply(completer, completed_args);
+            
+            expect(handler_args).toEqual(completed_args);
+        });
+        
+        it("should call each of the handlers associated with the Future", function() {
+            var future = completer.future(),
+                handler_1 = jasmine.createSpy('handler_1'),
+                handler_2 = jasmine.createSpy('handler_2'),
+                handler_3 = jasmine.createSpy('handler_3');
+                
+            future.then(handler_1).then(handler_2).then(handler_3);
+            
+            completer.complete();
+            
+            expect(handler_1).toHaveBeenCalled();
+            expect(handler_2).toHaveBeenCalled();
+            expect(handler_3).toHaveBeenCalled();
+        });
+    });
+});
